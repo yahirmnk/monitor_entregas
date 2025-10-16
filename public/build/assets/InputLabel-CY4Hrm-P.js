5d5ebf59b616c3c863fab365b81a21d0098a3838
Victor Yahir <corp.yahirmnk@gmail.com>
2025-10-16 01:45:50 -0600

    Solo npm build

---
+++ public/build/assets/InputLabel-CY4Hrm-P.js
@@ -0,0 +1,1 @@
+import{j as m}from"./app-Dm6ejG3y.js";function o({value:t,className:e="",children:r,...s}){return m.jsx("label",{...s,className:"block text-sm font-medium text-gray-700 "+e,children:t||r})}export{o as I};
