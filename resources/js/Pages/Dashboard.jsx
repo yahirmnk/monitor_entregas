04aa5add8a68ab63a8aa0403fd8030ec74e3c537
yahirmnk <corp.yahirmnk@gmail.com>
2025-10-16 11:47:38 -0600

    Correcion de filtro Salida Planta

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -277,7 +277,7 @@ export default function Dashboard() {
             <tbody>
               {movtos.length > 0 ? (
                 movtos
-                  //.filter((m) => !m.InicioRuta) //MANTENER DESACTIVADA YA QUE OMITE REGISTROS
+                  .filter((m) => !m.SalidaPlanta) //MANTENER DESACTIVADA YA QUE OMITE REGISTROS
                   .map((m, i) => {
                     const { colorCelda, colorFila } = getColorClase(m)
                     return (
